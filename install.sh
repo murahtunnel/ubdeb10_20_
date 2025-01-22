ff99d2b3fd1f094c83012cc12a86a47355e7c0f5
murahtunnel <dianvpn777@gmail.com>
2025-01-23 00:36:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -144,55 +144,6 @@ echo "OpenVZ is not supported"
 exit 1
 fi
 
-function pointingdomain() {
-
-    if [ -f /etc/xray/domain ] && [ -s /etc/xray/domain ]; then
-        echo "Domain sudah ada, melewati proses pointing."
-        return
-    fi
-
-    apt update
-    apt install jq curl -y
-    DOMAIN=klmpk.my.id
-    sub=$(head /dev/urandom | tr -dc a-z0-9 | head -c 8)
-    dns=${sub}.${DOMAIN}
-    CF_KEY=9d25535086484fb695ab64a70a70532a32fd4
-    CF_ID=andyyuda41@gmail.com
-    set -euo pipefail
-    echo ""
-    echo "Proses Pointing Domain ${dns}..."
-    sleep 1
-    ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-         -H "X-Auth-Email: ${CF_ID}" \
-         -H "X-Auth-Key: ${CF_KEY}" \
-         -H "Content-Type: application/json" | jq -r .result[0].id)
-
-    RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
-         -H "X-Auth-Email: ${CF_ID}" \
-         -H "X-Auth-Key: ${CF_KEY}" \
-         -H "Content-Type: application/json" | jq -r .result[0].id)
-
-    if [[ "${#RECORD}" -le 10 ]]; then
-         RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-         -H "X-Auth-Email: ${CF_ID}" \
-         -H "X-Auth-Key: ${CF_KEY}" \
-         -H "Content-Type: application/json" \
-         --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":true}' | jq -r .result.id)
-    fi
-
-    RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-         -H "X-Auth-Email: ${CF_ID}" \
-         -H "X-Auth-Key: ${CF_KEY}" \
-         -H "Content-Type: application/json" \
-         --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":true}')
-
-    # Menyimpan domain ke /etc/xray/domain hanya jika tidak ada
-    echo "$dns" > /etc/xray/domain
-    clear
-    echo ""
-    echo -e "\e[96;1mYour Domains:\e[0m ${dns}"
-}
-
 function MakeDirectories() {
     cd
     # Membuat direktori yang diperlukan
@@ -238,11 +189,56 @@ function MakeDirectories() {
     echo "& plughin Account" >>/etc/lunatic/vless/.vless.db
     echo "& plughin Account" >>/etc/lunatic/trojan/.trojan.db
     echo "& plughin Account" >>/etc/lunatic/ssh/.ssh.db
+}
+function pointingdomain() {
+    if [ -f /etc/xray/domain ] && [ -s /etc/xray/domain ]; then
+        echo "Domain sudah ada, melewati proses pointing."
+        return
+    fi
 
+    apt update
+    apt install jq curl -y
+    DOMAIN=klmpk.my.id
+    sub=$(head /dev/urandom | tr -dc a-z0-9 | head -c 8)
+    dns=${sub}.${DOMAIN}
+    CF_KEY=9d25535086484fb695ab64a70a70532a32fd4
+    CF_ID=andyyuda41@gmail.com
+    set -euo pipefail
+    echo ""
+    echo "Proses Pointing Domain ${dns}..."
+    sleep 1
+    ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+         -H "X-Auth-Email: ${CF_ID}" \
+         -H "X-Auth-Key: ${CF_KEY}" \
+         -H "Content-Type: application/json" | jq -r .result[0].id)
+
+    RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${dns}" \
+         -H "X-Auth-Email: ${CF_ID}" \
+         -H "X-Auth-Key: ${CF_KEY}" \
+         -H "Content-Type: application/json" | jq -r .result[0].id)
+
+    if [[ "${#RECORD}" -le 10 ]]; then
+         RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+         -H "X-Auth-Email: ${CF_ID}" \
+         -H "X-Auth-Key: ${CF_KEY}" \
+         -H "Content-Type: application/json" \
+         --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":true}' | jq -r .result.id)
+    fi
+
+    RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+         -H "X-Auth-Email: ${CF_ID}" \
+         -H "X-Auth-Key: ${CF_KEY}" \
+         -H "Content-Type: application/json" \
+         --data '{"type":"A","name":"'${dns}'","content":"'${IP}'","ttl":120,"proxied":true}')
+
+    # Menyimpan domain ke /etc/xray/domain hanya jika tidak ada
+    echo "$dns" > /etc/xray/domain
+    clear
+    echo ""
+    echo -e "\e[96;1mYour Domains:\e[0m ${dns}"
+MakeDirectories
 }
 
-# call function
-MakeDirectories
 pointingdomain
 
 # install dependencies
